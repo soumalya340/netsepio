664c42c10bd00ac687279d92a523d169cf741fd7
Soumalya Paul <samratghosh240@gmail.com>
2024-02-16 12:20:35 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,44 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const NetSepio = await hre.ethers.getContractFactory("NetSepio");
+  const netSepio = await NetSepio.deploy("NetSepio", "NETSEC");
+  console.log("NetSepio deployed to:", netSepio.address);
+  ///VERIFY
+  if (hre.network.name != "hardhat") {
+    await AccessMaster.deployTransaction.wait(6);
+    await verify(AccessMaster.address, [constructorParam.param1]);
+  }
+}
+
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+    Verified = true;
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
